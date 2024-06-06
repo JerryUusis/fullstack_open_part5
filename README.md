fbb9317c46a6dec4a336caea189ebe6e463d6439
JerryU <jerry.uusitalo@gmail.com>
2024-06-06 11:48:48 +0300

    Complete 5.17

--- README.md
+++ README.md
@@ -21,3 +21,6 @@
 - 5.14: blogilistan testit, step2 ✅
 - 5.15: blogilistan testit, step3 ✅
 - 5.16: blogilistan testit, step4 ✅
+
+## [Exercises 5.17-5.23](https://fullstackopen.com/osa5/end_to_end_testaus_playwright#tehtavat-5-17-5-23)
+- 5.17: blogilistan end to end ‑testit, step1 ✅
--- blogs-e2e/tests/loginForm.test.js
+++ blogs-e2e/tests/loginForm.test.js
@@ -1,20 +1,23 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Note app', () => {
-  beforeEach(async ({ page, request }) => {
-    await request.post('http:localhost:3003/api/testing/reset')
-    await request.post('http://localhost:3003/api/users', {
-      data: {
-        name: 'Matti Luukkainen',
-        username: 'mluukkai',
-        password: 'salainen'
-      }
+    beforeEach(async ({ page, request }) => {
+        await request.post('/api/testing/reset')
+        await request.post('/api/users', {
+            data: {
+                name: 'Matti Luukkainen',
+                username: 'mluukkai',
+                password: 'salainen'
+            }
+        })
+        await page.goto('/')
     })
 
-    await page.goto('http://localhost:5173')
-  })
-
-  test('Login form is shown', async ({ page }) => {
-    
-  })
+    test('Login form is shown', async ({ page }) => {
+        const locator = await page.getByText("Log in to application");
+        await expect(locator).toBeVisible();
+        await expect(page.getByText("Username")).toBeVisible()
+        await expect(page.getByText("Password")).toBeVisible()
+        await expect(page.getByText("Submit")).toBeVisible()
+    })
 })
